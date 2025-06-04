de58754eca6c142e27028d04c7340e139773c82b
JENebel <joachim.e.nebel@gmail.com>
2025-06-04 13:41:18 +0200

    Final fantasy

--- frontend/src/util.rs
+++ frontend/src/util.rs
@@ -1,5 +1,5 @@
 use gloo_console::info;
-use trip_tracker_lib::track_session::TrackSession;
+use trip_tracker_lib::{track_point::TrackPoint, track_session::TrackSession};
 
 pub fn haversine_distance(p1: (f64, f64), p2: (f64, f64)) -> f64 {
     const R: f64 = 6372.8; // Radius of the earth in km
@@ -25,7 +25,7 @@ pub fn filter_anomalies(mut session: TrackSession) -> TrackSession {
         return session;
     }
 
-    session.track_points.sort_by_key(|p| p.timestamp);
+    //session.track_points.sort_by_key(|p| p.timestamp);
 
     let mut prev_point = &session.track_points[0];
     let mut out = false;
@@ -36,21 +36,21 @@ pub fn filter_anomalies(mut session: TrackSession) -> TrackSession {
         // Calculate the distance between the two points
         let dist_to_prev = haversine_distance((prev_point.latitude, prev_point.longitude), (curr_point.latitude, curr_point.longitude));
         let dist_to_next = haversine_distance((curr_point.latitude, curr_point.longitude), (next_point.latitude, next_point.longitude));
-        let _neighbor_dist = haversine_distance((prev_point.latitude, prev_point.longitude), (next_point.latitude, next_point.longitude));
+        let neighbor_dist = haversine_distance((prev_point.latitude, prev_point.longitude), (next_point.latitude, next_point.longitude));
 
-        info!(&format!("{}", dist_to_prev));
-
-        // If the distance is too large, skip this point
-        if (!out && dist_to_prev > 0.1) || (out && dist_to_next > 0.1) {
-            out = !out;
+        if dist_to_prev + dist_to_next > neighbor_dist * 5. {
+            continue;
         }
 
-        if out {
+        if dist_to_prev > 5.0 {
+            continue;           
+        }
+
+        if filtered_points.iter().any(|p: &TrackPoint| p.latitude == curr_point.latitude && p.longitude == curr_point.longitude) {
             continue;
         }
 
         filtered_points.push(curr_point.clone());
-
         prev_point = curr_point;
     }
     
